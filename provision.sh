1795acbf2dc87e25c0281f0b6cf8ca9d934b8ae8
sickmoses <sickmoses@gmail.com>
2016-03-26 17:29:40 -0700

    updated path to nginx so that its on vagrant shared path

--- provision.sh
+++ provision.sh
@@ -2,9 +2,11 @@ apt-get -y update
 
 apt-get -y install nginx
 
+sudo rm -rf /usr/share/nginx/www/
+sudo ln -s /vagrant/www /usr/share/nginx/www
+
 service nginx start
 
-apt-get -y install  postgresql postgresql-contrib
 
 
 
